2d42baccc2e8fa0c8a2437b39f86e19ec794148d
hieuvnautotech <47775336+hieuvnautotech@users.noreply.github.com>
2024-04-09 16:56:52 +0700

    ok

---
+++ newpjedi/src/services/index.js
@@ -0,0 +1,28 @@
+import * as commonService from './Standard/Configuration/CommonService';
+import * as documentService from './Standard/Configuration/DocumentService';
+import * as loginService from './Login/LoginService';
+import * as menuService from './Standard/Configuration/MenuService';
+import * as permissionService from './Standard/Configuration/PermissionService';
+import * as roleService from './Standard/Configuration/RoleService';
+import * as userService from './Standard/Configuration/UserService';
+import * as versionAppService from './Standard/Configuration/VersionAppService';
+import * as q1ManagementService from './EDI/Q1ManagementService';
+import * as q2ManagementService from './EDI/Q2ManagementService';
+import * as policyService from './PolicyService';
+import * as machineService from './MachineService';
+import * as logFileService from './EDI/LogfileService';
+export {
+  commonService,
+  documentService,
+  loginService,
+  menuService,
+  permissionService,
+  roleService,
+  userService,
+  versionAppService,
+  q1ManagementService,
+  q2ManagementService,
+  policyService,
+  machineService,
+  logFileService,
+};
